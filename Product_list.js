d2df4e240505754635f26a1eff1b93d50d86bb42
monicxz <109973920+monicxz@users.noreply.github.com>
2022-07-26 23:36:29 +0800

    Add files via upload

---
+++ Product_list.js
@@ -0,0 +1,56 @@
+const data = {
+    "product": [{
+            "name": "BONE",
+            "brand": "rawrity",
+           "description": "big bone",
+            "price": "200",
+            "image_url": "https://media.istockphoto.com/photos/dog-rawhide-bone-picture-id183817025?k=20&m=183817025&s=612x612&w=0&h=lf35EprMK3QGhKI6LKbQkHYQRUN_DUH94ZvFK6i8DIA="
+            
+
+        },
+        {
+            "name": "BEEF JERKY",
+            "brand": "pedigree",
+"description": "200g",
+            "price": "280",
+            "image_url":"https://ph-test-11.slatic.net/p/4edaa107989bf15fd307a3e0673f645b.png" 
+        },
+        {
+            "name": "SQUIKY",
+            "brand": "toysRus",
+" description": "duck",
+            "price": "350",
+            "image_url":"https://m.media-amazon.com/images/I/61IoqaN0oVL.jpg" 
+        },
+        {
+            "name": "DOG FOOD",
+            "brand": "Royal Canin",
+"description": "10kg puppy",
+            "price": "1490",
+            "image_url": "https://cdn.royalcanin-weshare-online.io/fGnhrWsBG95Xk-RBpftc/v399/packshot-puppy-mini-shn17"
+           
+
+        },
+        {
+            "name": "BOWL",
+            "brand": "divis",
+"description": "silver large",
+            "price": "299",
+            "image_url": "https://media.istockphoto.com/photos/metal-pet-bowl-3d-picture-id1224049875?k=20&m=1224049875&s=612x612&w=0&h=-MZdnc2nLOZbbxhG3Tf5r9REjqyY6StidiXRqN26EHc="
+        },
+        {
+            "name": "WATER BOTTLE",
+            "brand": "shopee",
+" description": "portable blue",
+            "price": "480",
+            "image_url": "https://i0.wp.com/meandbrownee.com/wp-content/uploads/2021/03/2ee92fea6831794bf74fdef6960312d7.jpg?fit=1202%2C1500&ssl=1"
+         },
+        {
+            "name": "COLLAR",
+            "brand": "apple",
+" description": "iphone 11",
+            "price": "280",
+            "image_url": "https://images-na.ssl-images-amazon.com/images/I/6140xkCMmeL._AC_SL1000_.jpg"
+        }
+    ]
+}
